595ffdc43c8f7197fdb700dbaf55e75590bf3228
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-05-24 11:37:28 +0900

    EN_05

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -6,12 +6,14 @@ public class Goal : MonoBehaviour
 {
     public GameObject clearText;
     public GameObject nextButton;
+    public AudioSource audioSource;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("ÉSÅ[Éã");
 
         clearText.SetActive(true);
         nextButton.SetActive(true);
+        audioSource.Play();
     }
     // Start is called before the first frame update
     void Start()
