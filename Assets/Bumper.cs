acd372e39d64e0cb4aae43cc481788a4bdf04022
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-05-31 17:43:13 +0900

    06

---
+++ Assets/Bumper.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public float power = 10;
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody r = other.GetComponent<Rigidbody>();
+        if(r != null)
+        {
+            Vector3 angle = (other.transform.position
+                - transform.position).normalized;
+            r.velocity = angle * power;
+        }
+    }
+}
